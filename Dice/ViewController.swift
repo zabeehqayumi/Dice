530a08c52489363ec28436583e4722e47767269c
Zabeehullah Qayumi <32587905+zabeehqayumi@users.noreply.github.com>
2018-09-01 14:25:48 -0400

    initial commit

---
+++ Dice/ViewController.swift
@@ -0,0 +1,76 @@
+//
+//  ViewController.swift
+//  Dice
+//
+//  Created by Zabeehullah Qayumi on 7/31/18.
+//  Copyright © 2018 Zabeehullah Qayumi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    var randomDiceIndex1: Int = 0
+    var randomDiceIndex2: Int = 0
+    
+
+    
+    let diceArray : [String] = ["dice1", "dice2", "dice3", "dice4", "dice5", "dice6"]
+    
+    
+    @IBOutlet weak var diceImageView1: UIImageView!
+    @IBOutlet weak var diceImageView2: UIImageView!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        updatedDiceImages()
+        
+        
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func roolButtonPressed(_ sender: UIButton) {
+        updatedDiceImages()
+        
+   
+        
+       
+    }
+
+    
+    func updatedDiceImages(){
+        randomDiceIndex1 = Int(arc4random_uniform(6))
+        randomDiceIndex2 = Int(arc4random_uniform(6))
+
+        diceImageView1.image = UIImage(named: diceArray[randomDiceIndex1])
+        diceImageView2.image = UIImage(named: diceArray[randomDiceIndex2])
+
+    }
+
+    override func motionEnded(_ motion: UIEventSubtype, with event: UIEvent?) {
+
+        updatedDiceImages()
+    }
+
+ 
+ 
+    
+}
+
+
+
+
+
+
+
+
+
+
+
+
